4312b9a93eac391f59691f034d0176cc589778d6
hingyi khong and maly phongsavanh <hingyi@>
2018-08-08 09:32:22 -0700

    add a form to enter your name and a button to submit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#formName").submit(function(event) {
+    var nameInput  = $("input#yourName").val();
+
+    $("yourName").append();
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+});
